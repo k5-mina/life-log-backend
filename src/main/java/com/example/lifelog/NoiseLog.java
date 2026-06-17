9331e44f1652c574c21f13e43c75998f138f49b0
k5mina <any.minak.0122@gmail.com>
2026-06-18 00:15:21 +0900

    接続先DBをPostgreSQLに変更

--- src/main/java/com/example/lifelog/NoiseLog.java
+++ src/main/java/com/example/lifelog/NoiseLog.java
@@ -1,11 +1,9 @@
 package com.example.lifelog;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "noise_log")
 public class NoiseLog {
 
     @Id
